d939f95d5ae68f0de01c301d1ae39aa2ca2d57b3
OpenShift guest <dgodurli@gmail.com>
2013-08-12 13:16:32 -0400

    Other changes.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  attr_accessible :author, :text
+  attr_accessible :post_id, :author, :text
   validates_presence_of :author, message: 'You need to inform your name.'
   validates_presence_of :text, message: 'You need to post a message.'
 
